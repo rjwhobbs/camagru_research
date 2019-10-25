8f2993275a96583d1b9691183ec548eead18f7b2
Roger HOBBS <rhobbs@c4r23s4.wethinkcode.co.za>
2019-10-25 14:18:55 +0200

    Learning that js

---
+++ example/session.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+// $_SESSION['name'] = "musa";
+// var_dump($_SESSION);
+// die();
+//echo $_SESSION;
+
+var_dump($_GET);
+?>
---
+++ learnjs/myScript.js
@@ -0,0 +1,14 @@
+function myfunc()
+{
+	document.getElementById('demo').innerHTML = 1 + 1;
+}
+
+function kill()
+{
+	document.write('kill');
+}
+
+function alert()
+{
+	window.alert("Alert mofo!");
+}
