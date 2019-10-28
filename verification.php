0641d0cd6131101e4c9d5748d3170be929fdf96f
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-10-28 12:29:20 +0200

    Very basic logic behind verification works, time to implement it

--- verification.php
+++ verification.php
@@ -1,3 +1,26 @@
 <?php
+require_once ('./connection.php');
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['vcode']))
+{
+	try
+	{
+		$query = 'SELECT `username` FROM `users` WHERE verification = ?';
+		$stmt = $conn->prepare($query);
+		$stmt->execute([$_GET['vcode']]);
+		$res = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($res)
+		{
+			echo "Validation search works";
+		}
+		else
+		{
+			echo "Not validated";
+		}
 
+	}
+	catch (PDOException $e)
+	{
+		echo $e->getMessage();
+	}
+}
 ?>
