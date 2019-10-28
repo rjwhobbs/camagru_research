f97c1fa739c04f097cacbb440c036b21d765e825
Roger HOBBS <rhobbs@c4r6s4.wethinkcode.co.za>
2019-10-28 09:11:44 +0200

    So it appears the school has setup our postfix for us

--- mailtest.php
+++ mailtest.php
@@ -3,7 +3,7 @@ echo "mailtest<br>";
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 1);
 echo 'I am : ' . `whoami`;
-$headers = "From: mikethetrooper@gmail.com" . "\r\n";
+$headers = "From: rhobbs@student.wethinkcode.co.za" . "\r\n";
 $result = mail('rjw.hobbs@gmail.com','Testing 1 2 3','This is a test.', $headers);
 echo '<hr>Result was: ' . ( $result === FALSE ? 'FALSE' : 'TRUE') . $result;
 echo '<hr>';
