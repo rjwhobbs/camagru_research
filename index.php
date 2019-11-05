6a0e569303e47923f805d8cf4e1d5709fde2e7f2
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-05 07:06:50 +0200

    Testing git contributions

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 require ('./controller.php'); // Why didn't this work? was getting "session already started error"?
 require ('./header.php');
 ?>
-<h1>This is Camagru, the jankest site you'll ever use.</h1>
+<h1>This is Camagru.</h1>
 <h2>Feed will come here.</h2>
 <?php
 require ('./footer.php');
