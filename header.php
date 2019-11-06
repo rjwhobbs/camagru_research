c4810f57c0d536ea681078d3ab405b161d508b89
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-06 11:03:34 +0200

    Yes my eyes can breath

--- header.php
+++ header.php
@@ -12,10 +12,19 @@
 <body>
 	<h1>This is the header (place holder)</h1>
 	<?php
-		if (isset($_SESSION['user_id']))
-			require ('./src_signout.html');
-		else
-			require ('./src_signin.html'); 
+		if (isset($_SESSION['user_id'])) 
+		{	
+		?>
+			<a href="signout.php"><input type="submit" value="Sign Out"></a>
+			<a href="profile.php"><input type="submit" value="Profile"></a> 
+		<?php 
+		} 
+		else 
+		{	
+		?>
+			<a href="signin.php"><input type="submit" value="Sign In"></a>
+			<a href="form.php"><input type="submit" value="Sign Up"></a>
+		<?php 
+		}	
 	?>
-	<a href="profile.php"><input type="submit" value="Profile"></a>
 	<a href="index.php"><input type="submit" value="Feed"></a>
