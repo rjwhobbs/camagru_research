fe2518e1be4ceec9905316ee5a6f357c62cf8e49
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-11-07 18:53:29 +0200

    Basic edditing with php research

--- testeditor.php
+++ testeditor.php
@@ -1,10 +1,23 @@
 <?php
-$photo = imagecreatefrompng("images/test.png");
-$block = imagecreatetruecolor(100, 100);
-
-imagecopy($photo, $block, 0, 0, 0, 0, 100, 100);
 header('Content-Type: image/png');
+$photo = imagecreatefrompng("images/test.png");
+list($width, $height) = getimagesize("images/ChocChip.png");
 
+$new_width = $width * 0.2;
+$new_height = $height * 0.2;
+
+$sticker = imagecreatefrompng("images/ChocChip.png");
+
+//make canvas
+
+//$canvas = imagecreatetruecolor($new_width, $new_height);
+
+//imagecopyresampled($canvas, $sticker, 0, 0, 0, 0, 
+					//$new_width, $new_height, $width, $height);	
+
+//imagepng($sticker);
+$new_sticker = imagescale($sticker, $new_width, $new_height);
+imagecopy($photo, $new_sticker, 0, 0, 0, 0, $new_width, $new_height);
 imagepng($photo);
 imagedestroy($photo);
 ?>
