df200697127a569295c5e2d087725c66b16e3a0f
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-08 08:34:05 +0200

    Hard coded image edit

--- uploadpic.php
+++ uploadpic.php
@@ -2,26 +2,49 @@
 session_start();
 require ('./valid_session_check.php');
 require ('./connection.php');
+// if (isset($_POST['img']) && isset($_POST['edited']))
+// {
+// 	$user_id = $_SESSION['user_id'];
+// 	$edited = $_POST['edited'];
+
+// 	$img = $_POST['img'];
+// 	$img = str_replace('data:image/png;base64,', '', $img);
+// 	$img = str_replace(' ', '+', $img);
+// 	$data = base64_decode($img);
+// 	$file = "images/".uniqid().".png";
+// 	$success = file_put_contents($file, $data);
+
+// 	if ($success === FALSE)
+// 		echo "Couldn't upload";
+// 	else
+// 		echo "Successfully uploaded image.\n";
+	
+// 	$sql = 'INSERT INTO `images` (`path`, `user_id`, `edited` ) VALUES (?, ?, ?)';
+// 	$stmt = $conn->prepare($sql);
+// 	$stmt->execute([$file, $user_id, $edited]);
+// 	unset($stmt);	
+// }
 if (isset($_POST['img']) && isset($_POST['edited']))
 {
-	$user_id = $_SESSION['user_id'];
-	$edited = $_POST['edited'];
-
+	//$photo = imagecreatefrompng("images/test.png");
 	$img = $_POST['img'];
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
-	$data = base64_decode($img);
-	$file = "images/".uniqid().".png";
-	$success = file_put_contents($file, $data);
+	$data = base64_decode($img);	
+	$upload = imagecreatefromstring($data);
 
+	$sticker = imagecreatefrompng("images/chocchipsmall.png");
+	list($width, $height) = getimagesize("images/chocchipsmall.png");
+
+	imagecopy($upload, $sticker, 0, 0, 0, 0, $width, $height);
+	$file = "images/"."test".uniqid().".png";
+	$success = imagepng($upload, $file);
+	
 	if ($success === FALSE)
 		echo "Couldn't upload";
 	else
 		echo "Successfully uploaded image.\n";
-	
-	$sql = 'INSERT INTO `images` (`path`, `user_id`, `edited` ) VALUES (?, ?, ?)';
-	$stmt = $conn->prepare($sql);
-	$stmt->execute([$file, $user_id, $edited]);
-	unset($stmt);	
-}
+
+	imagedestroy($upload);
+}	
 ?>
