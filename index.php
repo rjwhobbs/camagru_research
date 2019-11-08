0f0cba8530773ca84b2cf23124ccf87c0902d91c
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-11-08 21:03:08 +0200

    Added basic comment form, think I should use AJAX, ie, it's not working at this commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 require ('./controller.php');
 require ('./header.php');
-include ('./query_functions.php')
+include ('./query_functions.php');
 ?>
 <h1>This is Camagru.</h1>
 <h2>Feed will come here.</h2>
@@ -14,6 +14,10 @@ include ('./query_functions.php')
 	{?>
 		<img src=<?php echo $images[$i]['path']; ?>><br>
 		<p>Upload by <?php echo get_image_author_name($images[$i]['user_id']) ?></p>
+		<form action="index.php" id="commentform">
+  			<input type="submit" name="add_comment" value="Comment">
+		</form>
+		<textarea rows="4" cols="50" name="comment" form="commentform" placeholder="Comment here"></textarea><br>
 		<?php $i++; ?>
 	<?php
 	}
