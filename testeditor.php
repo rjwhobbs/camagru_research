d9a6f570ec6029c6988d2683cd6acb3599934787
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-08 08:02:41 +0200

    Learnt how to save a image to a folder using imagepng

--- testeditor.php
+++ testeditor.php
@@ -1,12 +1,12 @@
 <?php
-header('Content-Type: image/png');
+//header('Content-Type: image/png');
 $photo = imagecreatefrompng("images/test.png");
-list($width, $height) = getimagesize("images/ChocChip.png");
 
-$new_width = $width * 0.2;
-$new_height = $height * 0.2;
+// $new_width = $width * 0.2;
+// $new_height = $height * 0.2;
 
-$sticker = imagecreatefrompng("images/ChocChip.png");
+$sticker = imagecreatefrompng("images/chocchipsmall.png");
+list($width, $height) = getimagesize("images/chocchipsmall.png");
 
 //make canvas
 
@@ -16,8 +16,16 @@ $sticker = imagecreatefrompng("images/ChocChip.png");
 					//$new_width, $new_height, $width, $height);	
 
 //imagepng($sticker);
-$new_sticker = imagescale($sticker, $new_width, $new_height);
-imagecopy($photo, $new_sticker, 0, 0, 0, 0, $new_width, $new_height);
-imagepng($photo);
+//$new_sticker = imagescale($sticker, $new_width, $new_height);
+imagecopy($photo, $sticker, 0, 0, 0, 0, $width, $height);
+
+$file = "images/"."test".uniqid().".png";
+$success = imagepng($photo, $file);
+
+if ($success === FALSE)
+	echo "Couldn't upload";
+else
+	echo "Successfully uploaded image.\n";
+
 imagedestroy($photo);
 ?>
