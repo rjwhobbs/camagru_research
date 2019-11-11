302b4532db766998764ea6bd7692d35d0f0d4e96
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-11-11 09:28:37 +0200

    edited email

--- mail_verification_code.php
+++ mail_verification_code.php
@@ -2,11 +2,10 @@
 
 function mail_verification_code($email, $code, $mode)
 {
-	$email_sender = "rhobbs@student.wethinkcode.co.za"; // DELETE ME LATER !!!!!!!!!!
-	//$email_sender = "mikethetrooper@gmail.com"; // DELETE ME LATER !!!!!!!!!!
+	$email_sender = "pmalope@student.wethinkcode.co.za"; // DELETE ME LATER !!!!!!!!!!
 	$headers = "From: $email_sender" . "\r\n";
 	if ($mode == "USER_VERIFY")
-		$link = 'http://localhost:8080/camagru_research/verification.php?vcode='.$code;
+		$link = 'http://localhost:8080/camagru/verification.php?vcode='.$code;
 	else if ($mode == "PASSWD_VERIFY")
 		$link = 'http://localhost:8080/camagru_research/reset.php?vcode='.$code;
 	if (mail($email, 'Email verification', $link, $headers) === FALSE)
--- setup.php
+++ setup.php
@@ -1,6 +1,8 @@
 <?php
-$DB_USER = 'test_root' ;
-$DB_PASSWORD = 's55oQsBuoDvyr2HB';
+// $DB_USER = 'test_root' ;
+// $DB_PASSWORD = 's55oQsBuoDvyr2HB';
+$DB_USER = 'root' ;
+$DB_PASSWORD = '143281';
 $DB_HOST = 'localhost';
 $DB_NAME = 'camagru';
 $DB_DSN = "mysql:host=$DB_HOST;dbname=$DB_NAME";
