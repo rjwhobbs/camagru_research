08771e226bd77c3d48f0e2de9f56458b31d7cba1
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-11-13 17:52:45 +0200

    Added email notification on likes

--- likes_manager.php
+++ likes_manager.php
@@ -3,6 +3,7 @@ session_start();
 //require ('./valid_session_check.php');
 require ('./connection.php');
 include ('./query_functions.php');
+include ('./mail_notif_function.php');
 if (isset($_POST['likes']) && isset($_POST['image_path']) && isset($_POST['image_id']) && isset($_SESSION['user_id']))
 {
 	//maybe some sanitization?
@@ -10,9 +11,9 @@ if (isset($_POST['likes']) && isset($_POST['image_path']) && isset($_POST['image
 	$image_path = $_POST['image_path'];
 	$user_id = $_SESSION['user_id'];
 
-	$query = "SELECT * FROM `images` WHERE `path` = ?";
+	$query = "SELECT * FROM `images` WHERE `path` = ? && `id` = ?";
 	$stmt = $conn->prepare($query);
-	$stmt->execute([$image_path]);
+	$stmt->execute([$image_path, $image_id]);
 	$res = $stmt->fetch(PDO::FETCH_ASSOC);
 	unset($stmt);
 	if ($res)
@@ -21,6 +22,7 @@ if (isset($_POST['likes']) && isset($_POST['image_path']) && isset($_POST['image
 		$likes += 1;
 		if ($image_id == $res['id'])
 		{
+			$image_owner_id = $res['user_id'];
 			$query = "SELECT `liked` FROM `likes` WHERE `image_id` = ? 
 						&& `user_id` = ?";
 			$stmt = $conn->prepare($query);
@@ -39,6 +41,15 @@ if (isset($_POST['likes']) && isset($_POST['image_path']) && isset($_POST['image
 							VALUE (?, ?, ?)";
 				$stmt = $conn->prepare($query);
 				$stmt->execute([$user_id, $image_id, 1]);
+				unset($stmt);
+				
+				$query = "SELECT * FROM `users` WHERE `id` = ?";
+				$stmt = $conn->prepare($query);
+				$stmt->execute([$image_owner_id]);
+				$res = $stmt->fetch(PDO::FETCH_ASSOC);
+
+				mail_like_notif($res['email']);
+
 				echo $likes;
 			}
 			else
---
+++ mail_notif_function.php
@@ -0,0 +1,10 @@
+<?php
+function mail_like_notif($owner_email)
+{
+	$email_sender = "rhobbs@student.wethinkcode.co.za"; // DELETE ME LATER !!!!!!!!!!
+	//$email_sender = "mikethetrooper@gmail.com"; // DELETE ME LATER !!!!!!!!!!
+	$headers = "From: $email_sender" . "\r\n";
+	$body = "Someone liked one of your pictures on Camagru.";
+	mail($owner_email, 'Someone liked your picture', $body, $headers);
+}
+?>
