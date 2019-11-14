764aef9d9d32206e88d0dc8804e5886a5e81dee6
Phetho MALOPE <pmalope@c4r3s1.wethinkcode.co.za>
2019-11-14 15:54:57 +0200

    added more HTML tags on the verification email

--- mail_verification_code.php
+++ mail_verification_code.php
@@ -7,7 +7,7 @@ function mail_verification_code($email, $code, $mode, $username)
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 	if ($mode == "USER_VERIFY") {
-		$link = '<h4>Welcome to camagru '. $username .'</h4><br>';
+		$link = '<h4>Welcome to camagru: '. $username .'</h4>';
 		$link .= '<p>Please confirm your account by clicking on the link below:<br>'.'http://localhost:8080/camagru/verification.php?vcode='.$code.'</p>';
 	}
 	else if ($mode == "PASSWD_VERIFY")
