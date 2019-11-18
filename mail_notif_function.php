d046513475b02214ed4e516d3483a7808ed3ade0
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-11-18 11:17:59 +0200

    Updated comment notif, too many query functions though

--- mail_notif_function.php
+++ mail_notif_function.php
@@ -10,16 +10,20 @@ function mail_like_notif($owner_email, $user ,$liker)
 
 	$body ='<h3>Hi '.$user.'</h3>';
 	$body .= '<p>'.$liker." liked one of your pictures on Camagru.</p>";
-	
+
 	mail($owner_email, 'Someone liked your picture', $body, $headers);
 }
 
-function mail_comment_notif($owner_email, $commenter)
+function mail_comment_notif($owner_email, $user ,$commenter)
 {
 	$email_sender = "rhobbs@student.wethinkcode.co.za"; // DELETE ME LATER !!!!!!!!!!
 	//$email_sender = "mikethetrooper@gmail.com"; // DELETE ME LATER !!!!!!!!!!
-	$headers = "From: $email_sender" . "\r\n";
-	$body =  $commenter." commented on one of your pictures on Camagru.";
+	$headers = "From: $email_sender"."\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+	$body ='<h3>Hi '.$user.'</h3>';
+	$body .='<p>'.$commenter." commented on one of your pictures on Camagru.</p>";
 	mail($owner_email, 'Someone commented on your picture', $body, $headers);
 }
 ?>
